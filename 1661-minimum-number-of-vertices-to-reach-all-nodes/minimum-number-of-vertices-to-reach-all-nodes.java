cb8ba5608b12bca54beced65d056e3afde835a96
itsayushman476 <itsayushman8853@gmail.com>
2026-02-19 22:21:59 +0530

    Time: 9 ms (98.44%) | Memory: 104.6 MB (79.50%) - LeetSync

---
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        int[] indegree = new int[n];
+        for(List<Integer> e : edges){
+            int to = e.get(1);
+            indegree[to]++;
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
