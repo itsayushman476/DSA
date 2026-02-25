41d9b76a05e3756cce22037b358d34edffd9a111
itsayushman476 <itsayushman8853@gmail.com>
2026-02-25 12:04:12 +0530

    Time: 514 ms (29.70%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,5 +1,9 @@
-SELECT activity_date AS day,
-COUNT(DISTINCT user_id) AS active_users
-FROM Activity
-WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
-GROUP BY activity_date; 
+SELECT 
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM (
+    SELECT *
+    FROM Activity
+    WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+) AS sub
+GROUP BY activity_date;
