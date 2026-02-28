cc0c37d28c0a99da33163fb14b221fbb6fcc7eb3
itsayushman476 <itsayushman8853@gmail.com>
2026-02-28 09:03:04 +0530

    Time: 174 ms (94.57%) | Memory: 169 MB (80.43%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
@@ -2,52 +2,45 @@ import java.util.*;
 
 class Solution {
     public int minTimeToReach(int[][] moveTime) {
+
         int m = moveTime.length;
         int n = moveTime[0].length;
 
-        int[][][] dist = new int[m][n][2];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                Arrays.fill(dist[i][j], Integer.MAX_VALUE);
-            }
+        int[][] dist = new int[m][n];
+        for(int[] row : dist){
+            Arrays.fill(row, Integer.MAX_VALUE);
         }
 
-        PriorityQueue<int[]> pq =
-                new PriorityQueue<>((a,b)->a[0]-b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
 
-        pq.add(new int[]{0,0,0,0}); // time,row,col,parity
-        dist[0][0][0] = 0;
+        pq.offer(new int[]{0,0,0}); // time,row,col
+        dist[0][0] = 0;
 
-        int[][] dir={{1,0},{-1,0},{0,1},{0,-1}};
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
 
         while(!pq.isEmpty()){
 
-            int[] cur=pq.poll();
-            int time=cur[0];
-            int r=cur[1];
-            int c=cur[2];
-            int parity=cur[3];
+            int[] cur = pq.poll();
+            int time = cur[0];
+            int r = cur[1];
+            int c = cur[2];
 
-            if(r==m-1 && c==n-1) return time;
+            if(r == m-1 && c == n-1) return time;
 
-            for(int[] d:dir){
+            for(int[] d : dir){
 
-                int nr=r+d[0];
-                int nc=c+d[1];
+                int nr = r + d[0];
+                int nc = c + d[1];
 
-                if(nr<0||nc<0||nr>=m||nc>=n) continue;
+                if(nr<0 || nc<0 || nr>=m || nc>=n) continue;
 
-                int moveCost = (parity==0)?1:2;
+                int moveCost = ((r + c) % 2 == 0) ? 1 : 2;
 
-                int newTime = Math.max(time,moveTime[nr][nc]) + moveCost;
+                int newTime = Math.max(time, moveTime[nr][nc]) + moveCost;
 
-                int newParity = 1-parity;
-
-                if(newTime < dist[nr][nc][newParity]){
-
-                    dist[nr][nc][newParity] = newTime;
-
-                    pq.offer(new int[]{newTime,nr,nc,newParity});
+                if(newTime < dist[nr][nc]){
+                    dist[nr][nc] = newTime;
+                    pq.offer(new int[]{newTime,nr,nc});
                 }
             }
         }
