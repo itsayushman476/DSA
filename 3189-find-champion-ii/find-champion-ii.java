e4cd8d65066ff9e0f05dfc2fce25fd559cb06d98
itsayushman476 <itsayushman8853@gmail.com>
2026-03-02 22:45:20 +0530

    Time: 1 ms (100.00%) | Memory: 48.3 MB (24.64%) - LeetSync

---
+++ 3189-find-champion-ii/find-champion-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findChampion(int n, int[][] edges) {
+
+        int[] indegree = new int[n];
+
+        for(int[] e : edges){
+            indegree[e[1]]++;
+        }
+
+        int champ = -1;
+
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                if(champ != -1) return -1;
+                champ = i;
+            }
+        }
+
+        return champ;
+    }
+}
